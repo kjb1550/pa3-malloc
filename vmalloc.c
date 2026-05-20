3aa50ebbe8a622753a807d8e2bc45478e3a5e2d1
Basa <kjbasa@ieng6-201.ucsd.edu>
2026-05-19 20:35:50 -0700

    Uploading files

--- vmalloc.c
+++ vmalloc.c
@@ -1,5 +1,24 @@
 #include "vm.h"
 #include "vmlib.h"
+#include <stddef.h>
+
+static struct block_header *find_best_fit(size_t needed_size) {
+    struct block_header *curr = heapstart;
+    struct block_header *best = NULL;
+
+    while (curr->size_status != VM_ENDMARK) {
+        size_t curr_sz = BLKSZ(curr);
+        int is_busy = curr->size_status & VM_BUSY;
+
+        if (!is_busy && curr_sz >= needed_size) {
+            if (best == NULL || curr_sz < BLKSZ(best)) {
+                best = curr;
+            }
+        }
+        curr = (struct block_header *)((char *)curr + curr_sz);
+    }
+    return best;
+}
 
 /**
  * Allocate a memory block with the given minimum size on the simulated heap.
@@ -10,6 +29,45 @@
  * function.
  */
 void *vmalloc(size_t size) {
-    // TODO
-    return NULL;
+    if (size == 0) return NULL;
+
+    size_t needed = ROUND_UP(size + sizeof(struct block_header), BLKALIGN);
+
+    struct block_header *blk = find_best_fit(needed);
+    if (blk == NULL) return NULL;
+
+    size_t blk_sz = BLKSZ(blk);
+    size_t leftover = blk_sz - needed;
+
+    if (leftover >= BLKALIGN) {
+        struct block_header *new_free =
+            (struct block_header *)((char *)blk + needed);
+
+        new_free->size_status = leftover | VM_PREVBUSY;
+
+        struct block_footer *new_footer =
+            (struct block_footer *)((char *)new_free + leftover - sizeof(struct block_footer));
+        new_footer->size = leftover;
+
+        struct block_header *after_new =
+            (struct block_header *)((char *)new_free + leftover);
+        if (after_new->size_status != VM_ENDMARK) {
+            after_new->size_status &= ~VM_PREVBUSY;
+        }
+
+        size_t prev_bit = blk->size_status & VM_PREVBUSY;
+        blk->size_status = needed | prev_bit | VM_BUSY;
+
+    } else {
+        size_t prev_bit = blk->size_status & VM_PREVBUSY;
+        blk->size_status = blk_sz | prev_bit | VM_BUSY;
+
+        struct block_header *nxt =
+            (struct block_header *)((char *)blk + blk_sz);
+        if (nxt->size_status != VM_ENDMARK) {
+            nxt->size_status |= VM_PREVBUSY;
+        }
+    }
+
+    return (void *)((char *)blk + sizeof(struct block_header));
 }
--- vmfree.c
+++ vmfree.c
@@ -1,5 +1,6 @@
 #include "vm.h"
 #include "vmlib.h"
+#include <stddef.h>
 
 /**
  * The vmfree() function frees the memory space pointed to by ptr,
@@ -10,5 +11,46 @@
  * If ptr is NULL, no operation is performed.
  */
 void vmfree(void *ptr) {
-    // TODO
+    if (ptr == NULL) return;
+
+    struct block_header *hdr =
+        (struct block_header *)((char *)ptr - sizeof(struct block_header));
+
+    if (!(hdr->size_status & VM_BUSY)) return;
+
+    size_t sz = BLKSZ(hdr);
+    size_t prev_bit = hdr->size_status & VM_PREVBUSY;
+
+    // --- Try to coalesce with the NEXT block ---
+    struct block_header *nxt =
+        (struct block_header *)((char *)hdr + sz);
+
+    if (nxt->size_status != VM_ENDMARK && !(nxt->size_status & VM_BUSY)) {
+        sz += BLKSZ(nxt);
+    }
+
+    // --- Try to coalesce with the PREVIOUS block ---
+    if (!(prev_bit)) {
+        struct block_footer *prev_footer =
+            (struct block_footer *)((char *)hdr - sizeof(struct block_footer));
+        size_t prev_sz = prev_footer->size;
+        struct block_header *prev_hdr =
+            (struct block_header *)((char *)hdr - prev_sz);
+
+        sz += prev_sz;
+        prev_bit = prev_hdr->size_status & VM_PREVBUSY;
+        hdr = prev_hdr; // merged block now starts here
+    }
+
+    hdr->size_status = sz | prev_bit;
+
+    struct block_footer *footer =
+        (struct block_footer *)((char *)hdr + sz - sizeof(struct block_footer));
+    footer->size = sz;
+
+    struct block_header *after =
+        (struct block_header *)((char *)hdr + sz);
+    if (after->size_status != VM_ENDMARK) {
+        after->size_status &= ~VM_PREVBUSY;
+    }
 }
